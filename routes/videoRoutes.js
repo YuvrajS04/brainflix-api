03f8db33343518a54b80feb6d4c194fd5da453a3
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2023-03-16 13:40:01 -0700

    add functionaility to backend

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,10 +1,19 @@
 const videoRouter = require("express").Router();
 const fs = require("fs");
+const express = require("express");
+const app = express();
+const { v4: uuid } = require("uuid");
+
 
 function readVideoData() {
   const videoFile = fs.readFileSync("./data/video-details.json");
   const videosData = JSON.parse(videoFile);
   return videosData;
+}
+
+function writeVideoData(data) {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/video-details.json", stringifiedData);
 }
 
 videoRouter.get("/", (req, res) => {
@@ -19,7 +28,7 @@ videoRouter.get("/", (req, res) => {
       channel: video.channel,
     };
   });
-  console.log(videos);
+  // console.log(videos);
   res.status(200).json(videos);
 });
 
@@ -30,9 +39,35 @@ videoRouter.get("/:id", (req, res) => {
   const selectedVideoData = videoData.find(
     (video) => video.id == req.params.id
   );
-  console.log(selectedVideoData);
+  // console.log(selectedVideoData);
 
   res.status(200).json(selectedVideoData);
 });
+
+videoRouter.post("/", (req, res) => {
+  const videoPostData = readVideoData();
+
+  const newVideo = {
+    id: uuid(),
+    title: req.body.title,
+    channel: "Yuvi",
+    image: "",
+    description: req.body.description,
+    views: "0",
+    likes: "0",
+    duration: "6:69",
+    video: "",
+    timestamp: Date.now(),
+    comments: []
+  }
+  console.log(newVideo);
+  console.log("reached backend");
+
+  videoPostData.push(newVideo);
+  writeVideoData(videoPostData);
+  res.status(200).send();
+
+});
+
 
 module.exports = videoRouter;
